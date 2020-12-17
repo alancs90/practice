c13786aa00649f7e4584cedf0961715f7ea6615f
alancs90 <alancs90@gmial.com>
2020-12-17 01:28:42 -0500

    max num

---
+++ maxNum.js
@@ -0,0 +1,10 @@
+function maxNum(num1, num2){
+    // if(num1>num2) return num1;
+    // return num2;
+  
+    return(num1>num2)? num1 : num2;
+  }
+  
+  let num = maxNum(5,10);
+  console.log(num);
+  
