8ccd12636e7ef8f2afeed7cb23b6fcb219626e08
alancs90 <alancs90@gmial.com>
2020-12-17 01:49:30 -0500

    isLandscape

---
+++ landscapeOrPortrait.js
@@ -0,0 +1,10 @@
+console.log(isLandscape(500,200));
+
+function isLandscape(width, height){
+
+// return(width > height) ? true : false;
+
+// same thing, better code
+return (width > height);
+    
+}
