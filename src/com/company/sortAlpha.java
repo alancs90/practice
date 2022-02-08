830f27a300e8edd7c6e454d97e30ce742ff36aaf
Alan Carrasco <alancs90@gmail.com>
2022-02-07 22:05:27 -0500

    auto-update 2022-02-07-22:05:27

---
+++ src/com/company/sortAlpha.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class sortAlpha {
+
+    public static void main(String[] args) {
+        sortAlpha("October");
+    }
+
+    public static String sortAlpha(String str){
+      char[] chars =  str.toLowerCase().toCharArray();
+
+        Arrays.sort(chars);
+        System.out.println(Arrays.toString(chars));
+//        String str1 = new String(chars);
+//        return str1;
+        String result = "";
+        for (char letter: chars){
+            result += letter;
+        }
+        for (int i = 0; i < chars.length; i++){
+                result += chars[i] ;
+        }
+        int i = 0;
+        while ( i < chars.length){
+            result += chars[i] ;
+            i++;
+        }
+        System.out.println(result);
+        return result;
+    }
+
+}
