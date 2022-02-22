0288c25b9679dcf22c303c3ca6dc43ab20e3edaf
Alan Carrasco <alancs90@gmail.com>
2022-02-22 16:20:22 -0500

    auto-update 2022-02-22-16:20:21

--- src/com/company/BuySellStockII.java
+++ src/com/company/BuySellStockII.java
@@ -2,19 +2,25 @@ package com.company;
 
 public class BuySellStockII {
     public static void main(String[] args) {
-        int[] arr ={7,6,4,3,1};
+        int[] arr ={3,3,5,7,9};
         maxProfit(arr);
     }
 
     public static int maxProfit(int[] prices){
         int profit = 0;
 
-        for (int i = 0; i < prices.length-1 ; i++) {
-            if (prices[i+1] > prices[i]) {
-                profit +=  prices[i+1] - prices[i];
+        for (int i = 0; i < prices.length-1; i++){
+            if (prices[i+1] > prices[i]){
+                profit = profit + prices[i+1] - prices[i];
             }
         }
+//        for (int i = 0; i < prices.length-1 ; i++) {
+//            if (prices[i+1] > prices[i]) {
+//                profit +=  prices[i+1] - prices[i];
+//            }
+//        }
         System.out.println(profit);
+        
         return profit;
     }
 }
