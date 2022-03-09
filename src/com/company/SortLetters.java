5196daa6020c61d515a123d78601750955e15316
Alan Carrasco <alancs90@gmail.com>
2022-03-08 22:16:35 -0500

    auto-update 2022-03-08-22:16:35

--- src/com/company/SortLetters.java
+++ src/com/company/SortLetters.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Locale;
+import java.util.*;
 
 public class SortLetters {
     public static void main(String[] args) {
@@ -13,9 +12,19 @@ public class SortLetters {
         char[] letters = s.toLowerCase().toCharArray();
         Arrays.sort(letters);
         String sortedString = "";
+        Set<Character> set = new LinkedHashSet<>();
         for (int i = 0; i < letters.length; i++){
             sortedString = sortedString + letters[i];
+            set.add(letters[i]);
         }
+        List<Character> list = new ArrayList<>(set);
+        String str = "";
+        for (char c: set){
+            str = str + c;
+        }
+        System.out.println(list);
+        System.out.println(str);
+        System.out.println(set.toString());
         System.out.println(sortedString);
         return sortedString;
     }
